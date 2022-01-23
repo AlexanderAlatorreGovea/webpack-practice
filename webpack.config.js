9086b3e6b396af569ed23fac031607d13a2c8073
Alexander <Alexander_alatorre1993@hotmail.com>
2022-01-22 16:45:31 -0800

    changed package

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      }
     ],
   },
   plugins: [
