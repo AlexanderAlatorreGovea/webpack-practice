e31b29be4922befb30024a62f3148e2b84b313e6
Alexander <Alexander_alatorre1993@hotmail.com>
2022-01-22 16:06:56 -0800

    configed webpack

---
+++ src/public/js/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+const START_UP_BOILER_PLATE = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ReactDOM.render(START_UP_BOILER_PLATE, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const WebpackHtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "public", "js", "main.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackHtmlPlugin({
+      template: path.join(__dirname, "src", "public", "index.html"),
+    }),
+  ],
+};
